050e3b5b51244724cc366009fa2b4ac22849a0c3
wu-tai-ting <taes10122@gmail.com>
2025-12-11 19:31:03 +0800

    資料庫

---
+++ order_list.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-12-11 11:57:22
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `kirby cafe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `order_list`
+--
+
+CREATE TABLE `order_list` (
+  `id` int(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `food` varchar(50) NOT NULL,
+  `total_price` int(11) NOT NULL,
+  `note` varchar(100) NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `order_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
